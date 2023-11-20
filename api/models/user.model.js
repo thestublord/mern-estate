636357bcc55a6aee073dd1becfeb2dd460f9b674
Nicholas VanBuskirk <thestublord@gmail.com>
2023-11-20 05:32:20 -0700

    created sign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    usename: {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
       type: String,
       required: true,
       unique: true,
