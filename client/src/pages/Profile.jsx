aff5d1093882b4fdf1e952652d84a8299a94c1d9
Nicholas VanBuskirk <thestublord@gmail.com>
2023-12-09 04:10:01 -0700

    created show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -24,6 +24,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [shoowListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   //firebase storage
@@ -126,6 +128,22 @@ export default function Profile() {
     }
   };
 
+  const handleShowLisings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+      return;
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center m-7">Profile</h1>
@@ -205,6 +223,46 @@ export default function Profile() {
       <p className="text-green-700">
         {updateSuccess ? "User is updated succeffully!" : ""}
       </p>
+      <button onClick={handleShowLisings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {shoowListingsError ? "Error showing listings" : ""}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center text-2xl font-semibold">Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className=" p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contains"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+                to={`listing/${listing._id}`}
+              >
+                <p>{listing.title}</p>
+              </Link>
+              <div className="flex flex-col items-center'">
+                <Link
+                  className="text-green-700 hover:underline"
+                  to={`/edit-listing/${listing._id}`}
+                >
+                  Edit
+                </Link>
+                <span className="text-red-700 cursor-pointer">Delete</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
